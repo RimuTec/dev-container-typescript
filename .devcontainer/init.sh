8cc3155d4662e6e38f17c5d9bc6189590b9417be
Manfred Lange <manfredmlange@gmail.com>
2023-02-12 12:54:00 +1300

    upgrade node, npm and typescript

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+# #!/bin/sh
+
+echo Running script ./.devcontainer/init.sh
 
 ################################################################################
 # Set get pull mode to rebase. Do the same for branches that may exist. However
 # this is a repo designed for trunk-based development, so there shouldn't be any
 # branches other than 'main' [Manfred, 22aug2022]
 # For more details, see https://stackoverflow.com/a/13974638/411428
-git config pull.rebase true
-git config branch.autosetuprebase always
+runuser -l node -c 'cd /work && git config pull.rebase true'
+runuser -l node -c 'cd /work && git config branch.autosetuprebase always'
+# option -l specifies the user on whose behalf the commmand is executed. Note that
+#           this script runs as root. [Manfred, 12 Feb 2023]
+
+echo Script ./.devcontainer/init.sh finished
